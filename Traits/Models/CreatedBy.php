5350e7e536d3dc217d891c988e4b0184dc3a2199
Boris Blondin <blondin.boris@gmail.com>
2020-01-26 06:49:56 +0000

    Add install migrations

--- Traits/Models/CreatedBy.php
+++ Traits/Models/CreatedBy.php
@@ -3,16 +3,23 @@
 namespace Pingu\Core\Traits\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Pingu\User\Entities\User;
 
 trait CreatedBy
 {
+    /**
+     * Created by relationship
+     * 
+     * @return BelongsTo
+     */
     public function createdBy()
     {
         return $this->belongsTo(User::class, 'created_by');
     }
 
+    /**
+     * Boots trait. Insert current user in created by field
+     */
     public static function bootCreatedBy()
     {
         static::saving(
